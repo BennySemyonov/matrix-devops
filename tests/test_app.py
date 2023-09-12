aceb32abd1a0ea7759f950ba75fb9d44b0151ece
BennySemyonov <bennysemyonov@gmail.com>
2023-09-12 23:54:28 +0300

    changed test to fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ from main import app
 def test_home_route():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello World" in response.data
+    assert b"Hello World!" in response.data
